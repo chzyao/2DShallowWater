20687ec491c7cad415e94bba0a262408c81150fa
Chris Ziyi Yao <chris.yao20@imperial.ac.uk>
2023-03-20 16:23:56 +0000

    Serial code draft_MPI_v3 with new conventions

--- MPI/draft_MPI_v3.cpp
+++ MPI/draft_MPI_v3.cpp
@@ -3,43 +3,41 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <mpi.h>
+// #include <mpi.h>
 #include <cstdlib>
 #include <cblas.h>
 
-#include "ShallowWater.h"
 
 using namespace std;
 namespace po = boost::program_options;
 
-
 void SetInitialConditions(double *u, double *v, double *h, double *h0, int Nx,
                           int Ny, int ic, double dx, double dy)
 {
-    for (int i = 0; i < Ny; ++i)
+    for (int i = 0; i < Nx; ++i)
     {
-        for (int j = 0; j < Nx; ++j)
+        for (int j = 0; j < Ny; ++j)
         {
             // All coded in row-major for now
-            u[i * Nx + j] = 0.0;
-            v[i * Nx + j] = 0.0;
+            u[i * Ny + j] = 0.0;
+            v[i * Ny + j] = 0.0;
             if (ic == 1)
             {
-                h0[i * Nx + j] = 10.0 + exp(-(i * dx - 50) * (i * dx - 50) / 25.0);
+                h0[i * Ny + j] = 10.0 + exp(-(i * dx - 50) * (i * dx - 50) / 25.0);
             }
             else if (ic == 2)
             {
-                h0[i * Nx + j] = 10.0 + exp(-(j * dy - 50) * (j * dy - 50) / 25.0);
+                h0[i * Ny + j] = 10.0 + exp(-(j * dy - 50) * (j * dy - 50) / 25.0);
             }
             else if (ic == 3)
             {
-                h0[i * Nx + j] = 10.0 + exp(
+                h0[i * Ny + j] = 10.0 + exp(
                                             -((i * dx - 50) * (i * dx - 50) + (j * dy - 50) * (j * dy - 50)) /
                                             25.0);
             }
             else
             {
-                h0[i * Nx + j] = 10.0 + exp(-((i * dx - 25) * (i * dx - 25) + (j * dy - 25) * (j * dy - 25)) / 25.0) +
+                h0[i * Ny + j] = 10.0 + exp(-((i * dx - 25) * (i * dx - 25) + (j * dy - 25) * (j * dy - 25)) / 25.0) +
                                  exp(-((i * dx - 75) * (i * dx - 75) + (j * dy - 75) * (j * dy - 75)) / 25.0);
             }
         }
@@ -57,16 +55,16 @@ void SpatialDiscretisation(double *u, int Nx, int Ny, double dx, double dy,
     {
         double px = 1.0 / dx;
 
-        for (int j = 0; j < Nx; ++j)
+        for (int i = 0; i < Nx; ++i)
         {
 
-            for (int i = 0; i < Ny; ++i)
+            for (int j = 0; j < Ny; ++j)
             {
-                deriv[i * Nx + j] =
+                deriv[i * Ny + j] =
                     px *
-                    (-u[((i - 3 + Nx) % Nx) * Nx + j] / 60.0 + 3.0 / 20.0 * u[((i - 2 + Nx) % Nx) * Nx + j] -
-                     3.0 / 4.0 * u[((i - 1 + Nx) % Nx) * Nx + j] + 3.0 / 4.0 * u[((i + 1) % Nx) * Nx + j] -
-                     3.0 / 20.0 * u[((i + 2) % Nx) * Nx + j] + u[((i + 3) % Nx) * Nx + j] / 60.0);
+                    (-u[((i - 3 + Nx) % Nx) * Ny + j] / 60.0 + 3.0 / 20.0 * u[((i - 2 + Nx) % Nx) * Ny + j] -
+                     3.0 / 4.0 * u[((i - 1 + Nx) % Nx) * Ny + j] + 3.0 / 4.0 * u[((i + 1) % Nx) * Ny + j] -
+                     3.0 / 20.0 * u[((i + 2) % Nx) * Ny + j] + u[((i + 3) % Nx) * Ny + j] / 60.0);
             }
         }
     }
@@ -76,21 +74,20 @@ void SpatialDiscretisation(double *u, int Nx, int Ny, double dx, double dy,
     {
         double py = 1.0 / dy;
 
-        for (int i = 0; i < Ny; ++i)
+        for (int i = 0; i < Nx; ++i)
         {
 
-            for (int j = 0; j < Nx; ++j)
+            for (int j = 0; j < Ny; ++j)
             {
-                deriv[i * Nx + j] =
+                deriv[i * Ny + j] =
                     py *
-                    (-u[i * Nx + (j - 3 + Ny) % Ny] / 60.0 + 3.0 / 20.0 * u[i * Nx + (j - 2 + Ny) % Ny] -
-                     3.0 / 4.0 * u[i * Nx + (j - 1 + Ny) % Ny] + 3.0 / 4.0 * u[i * Nx + (j + 1) % Ny] -
-                     3.0 / 20.0 * u[i * Nx + (j + 2) % Ny] + u[i * Nx + (j + 3) % Ny] / 60.0);
+                    (-u[i * Ny + (j - 3 + Ny) % Ny] / 60.0 + 3.0 / 20.0 * u[i * Ny + (j - 2 + Ny) % Ny] -
+                     3.0 / 4.0 * u[i * Ny + (j - 1 + Ny) % Ny] + 3.0 / 4.0 * u[i * Ny + (j + 1) % Ny] -
+                     3.0 / 20.0 * u[i * Ny + (j + 2) % Ny] + u[i * Ny + (j + 3) % Ny] / 60.0);
             }
         }
     }
 }
-
 
 void Evaluate_fu(double *u, double *v, double *h, int Nx, int Ny,
                  double dx, double dy, double *f)
@@ -104,13 +101,13 @@ void Evaluate_fu(double *u, double *v, double *h, int Nx, int Ny,
     SpatialDiscretisation(u, Nx, Ny, dx, dy, 'y', deriuy);
     SpatialDiscretisation(h, Nx, Ny, dx, dy, 'x', derihx);
 
-    for (int i = 0; i < Ny; ++i)
+    for (int i = 0; i < Nx; ++i)
     {
-        for (int j = 0; j < Nx; ++j)
+        for (int j = 0; j < Ny; ++j)
         {
-            f[i * Nx + j] = -u[i * Nx + j] * deriux[i * Nx + j] -
-                            v[i * Nx + j] * deriuy[i * Nx + j] -
-                            g * derihx[i * Nx + j];
+            f[i * Ny + j] = -u[i * Ny + j] * deriux[i * Ny + j] -
+                            v[i * Ny + j] * deriuy[i * Ny + j] -
+                            g * derihx[i * Ny + j];
         }
     }
 
@@ -131,13 +128,13 @@ void Evaluate_fv(double *u, double *v, double *h, int Nx, int Ny,
     SpatialDiscretisation(v, Nx, Ny, dx, dy, 'y', derivy);
     SpatialDiscretisation(h, Nx, Ny, dx, dy, 'y', derihy);
 
-    for (int i = 0; i < Ny; ++i)
+    for (int i = 0; i < Nx; ++i)
     {
-        for (int j = 0; j < Nx; ++j)
+        for (int j = 0; j < Ny; ++j)
         {
-            f[i * Nx + j] = -u[i * Nx + j] * derivx[i * Nx + j] -
-                            v[i * Nx + j] * derivy[i * Nx + j] -
-                            g * derihy[i * Nx + j];
+            f[i * Ny + j] = -u[i * Ny + j] * derivx[i * Ny + j] -
+                            v[i * Ny + j] * derivy[i * Ny + j] -
+                            g * derihy[i * Ny + j];
         }
     }
 
@@ -155,23 +152,23 @@ void Evaluate_fh(double *u, double *v, double *h, int Nx, int Ny,
     double *hv = new double[Nx * Ny];
 
     // find hu and hv
-    for (int i = 0; i < Ny; ++i)
+    for (int i = 0; i < Nx; ++i)
     {
-        for (int j = 0; j < Nx; ++j)
+        for (int j = 0; j < Ny; ++j)
         {
-            hu[i * Nx + j] = h[i * Nx + j] * u[i * Nx + j];
-            hv[i * Nx + j] = h[i * Nx + j] * v[i * Nx + j];
+            hu[i * Ny + j] = h[i * Ny + j] * u[i * Ny + j];
+            hv[i * Ny + j] = h[i * Ny + j] * v[i * Ny + j];
         }
     }
 
     SpatialDiscretisation(hu, Nx, Ny, dx, dy, 'x', derihux);
     SpatialDiscretisation(hv, Nx, Ny, dx, dy, 'y', derihvy);
 
-    for (int i = 0; i < Ny; ++i)
+    for (int i = 0; i < Nx; ++i)
     {
-        for (int j = 0; j < Nx; ++j)
+        for (int j = 0; j < Ny; ++j)
         {
-            f[i * Nx + j] = -derihux[i * Nx + j] - derihvy[i * Nx + j];
+            f[i * Ny + j] = -derihux[i * Ny + j] - derihvy[i * Ny + j];
         }
     }
 
@@ -181,6 +178,153 @@ void Evaluate_fh(double *u, double *v, double *h, int Nx, int Ny,
     delete[] hv;
 }
 
+void TimeIntegration(double *u, double *v, double *h, int Nx, int Ny,
+                     double dx, double dy, double dt, double *fu,
+                     double *fv, double *fh)
+{
+    // Solving for u
+    double *k1_u = new double[Nx * Ny];
+    double *k2_u = new double[Nx * Ny];
+    double *k3_u = new double[Nx * Ny];
+    double *k4_u = new double[Nx * Ny];
+
+    // Solve for v
+    double *k1_v = new double[Nx * Ny];
+    double *k2_v = new double[Nx * Ny];
+    double *k3_v = new double[Nx * Ny];
+    double *k4_v = new double[Nx * Ny];
+
+    // Solve for h
+    double *k1_h = new double[Nx * Ny];
+    double *k2_h = new double[Nx * Ny];
+    double *k3_h = new double[Nx * Ny];
+    double *k4_h = new double[Nx * Ny];
+
+    double *tu = new double[Nx * Ny]; // temp vector t = u
+    double *tv = new double[Nx * Ny]; // temp vector t = v
+    double *th = new double[Nx * Ny]; // temp vector t = h
+
+    // Calculating k1 = f(yn) ===================================
+    cblas_dcopy(Nx * Ny, u, 1, tu, 1);
+    cblas_dcopy(Nx * Ny, v, 1, tv, 1);
+    cblas_dcopy(Nx * Ny, h, 1, th, 1);
+
+    Evaluate_fu(u, v, h, Nx, Ny, dx, dy, fu);
+    Evaluate_fv(u, v, h, Nx, Ny, dx, dy, fv);
+    Evaluate_fh(u, v, h, Nx, Ny, dx, dy, fh);
+
+    // Evaluate_fu_BLAS(u, v, h, Nx, Ny, dx, dy, fu);
+    // Evaluate_fv_BLAS(u, v, h, Nx, Ny, dx, dy, fv);
+    // Evaluate_fh_BLAS(u, v, h, Nx, Ny, dx, dy, fh);
+
+    cblas_dcopy(Nx * Ny, fu, 1, k1_u, 1);
+    cblas_dcopy(Nx * Ny, fv, 1, k1_v, 1);
+    cblas_dcopy(Nx * Ny, fh, 1, k1_h, 1);
+
+    // Calculating k2 = f(yn + dt*k1/2) ==========================
+    // reset temp values
+    cblas_dcopy(Nx * Ny, u, 1, tu, 1); // reset tu to u
+    cblas_dcopy(Nx * Ny, v, 1, tv, 1);
+    cblas_dcopy(Nx * Ny, h, 1, th, 1);
+
+    // update un to un+dt*k1/2 to evaluate f for k2
+    cblas_daxpy(Nx * Ny, dt / 2.0, k1_u, 1, tu, 1);
+    cblas_daxpy(Nx * Ny, dt / 2.0, k1_v, 1, tv, 1);
+    cblas_daxpy(Nx * Ny, dt / 2.0, k1_h, 1, th, 1);
+
+    // Evaluate new f
+    Evaluate_fu(tu, tv, th, Nx, Ny, dx, dy, fu);
+    Evaluate_fv(tu, tv, th, Nx, Ny, dx, dy, fv);
+    Evaluate_fh(tu, tv, th, Nx, Ny, dx, dy, fh);
+
+    // Evaluate_fu_BLAS(tu, tv, th, Nx, Ny, dx, dy, fu);
+    // Evaluate_fv_BLAS(tu, tv, th, Nx, Ny, dx, dy, fv);
+    // Evaluate_fh_BLAS(tu, tv, th, Nx, Ny, dx, dy, fh);
+
+    cblas_dcopy(Nx * Ny, fu, 1, k2_u, 1);
+    cblas_dcopy(Nx * Ny, fv, 1, k2_v, 1);
+    cblas_dcopy(Nx * Ny, fh, 1, k2_h, 1);
+
+    // Calculating k3 = f(yn+dt*k2/2) =============================
+    // reset temp values
+    cblas_dcopy(Nx * Ny, u, 1, tu, 1); // reset tu to u
+    cblas_dcopy(Nx * Ny, v, 1, tv, 1);
+    cblas_dcopy(Nx * Ny, h, 1, th, 1);
+
+    // update un to un+dt*k2/2 to evaluate f for k3
+    cblas_daxpy(Nx * Ny, dt / 2.0, k2_u, 1, tu, 1);
+    cblas_daxpy(Nx * Ny, dt / 2.0, k2_v, 1, tv, 1);
+    cblas_daxpy(Nx * Ny, dt / 2.0, k2_h, 1, th, 1);
+
+    Evaluate_fu(tu, tv, th, Nx, Ny, dx, dy, fu);
+    Evaluate_fv(tu, tv, th, Nx, Ny, dx, dy, fv);
+    Evaluate_fh(tu, tv, th, Nx, Ny, dx, dy, fh);
+
+    // Evaluate_fu_BLAS(tu, tv, th, Nx, Ny, dx, dy, fu);
+    // Evaluate_fv_BLAS(tu, tv, th, Nx, Ny, dx, dy, fv);
+    // Evaluate_fh_BLAS(tu, tv, th, Nx, Ny, dx, dy, fh);
+
+    cblas_dcopy(Nx * Ny, fu, 1, k3_u, 1);
+    cblas_dcopy(Nx * Ny, fv, 1, k3_v, 1);
+    cblas_dcopy(Nx * Ny, fh, 1, k3_h, 1);
+
+    // k4 = f(yn+dt*k3) ===========================================
+    // reset temp values
+    cblas_dcopy(Nx * Ny, u, 1, tu, 1); // reset tu to u
+    cblas_dcopy(Nx * Ny, v, 1, tv, 1);
+    cblas_dcopy(Nx * Ny, h, 1, th, 1);
+
+    // update un to un+dt*k2/2 to evaluate f for k3
+    cblas_daxpy(Nx * Ny, dt, k3_u, 1, tu, 1);
+    cblas_daxpy(Nx * Ny, dt, k3_v, 1, tv, 1);
+    cblas_daxpy(Nx * Ny, dt, k3_h, 1, th, 1);
+
+    Evaluate_fu(tu, tv, th, Nx, Ny, dx, dy, fu);
+    Evaluate_fv(tu, tv, th, Nx, Ny, dx, dy, fv);
+    Evaluate_fh(tu, tv, th, Nx, Ny, dx, dy, fh);
+
+    // Evaluate_fu_BLAS(tu, tv, th, Nx, Ny, dx, dy, fu);
+    // Evaluate_fv_BLAS(tu, tv, th, Nx, Ny, dx, dy, fv);
+    // Evaluate_fh_BLAS(tu, tv, th, Nx, Ny, dx, dy, fh);
+
+    cblas_dcopy(Nx * Ny, fu, 1, k4_u, 1);
+    cblas_dcopy(Nx * Ny, fv, 1, k4_v, 1);
+    cblas_dcopy(Nx * Ny, fh, 1, k4_h, 1);
+
+    // yn+1 = yn + 1/6*(k1+2*k2+2*k3+k4)*dt
+    // Update solution
+    for (int i = 0; i < Ny; ++i)
+    {
+        for (int j = 0; j < Nx; ++j)
+        {
+            u[i * Nx + j] += dt / 6.0 *
+                             (k1_u[i * Nx + j] + 2.0 * k2_u[i * Nx + j] +
+                              2.0 * k3_u[i * Nx + j] + k4_u[i * Nx + j]);
+            v[i * Nx + j] += dt / 6.0 *
+                             (k1_v[i * Nx + j] + 2.0 * k2_v[i * Nx + j] +
+                              2.0 * k3_v[i * Nx + j] + k4_v[i * Nx + j]);
+            h[i * Nx + j] += dt / 6.0 *
+                             (k1_h[i * Nx + j] + 2.0 * k2_h[i * Nx + j] +
+                              2.0 * k3_h[i * Nx + j] + k4_h[i * Nx + j]);
+        }
+    }
+
+    // deallocate memory
+    delete[] k1_u;
+    delete[] k2_u;
+    delete[] k3_u;
+    delete[] k4_u;
+
+    delete[] k1_v;
+    delete[] k2_v;
+    delete[] k3_v;
+    delete[] k4_v;
+
+    delete[] k1_h;
+    delete[] k2_h;
+    delete[] k3_h;
+    delete[] k4_h;
+}
 
 int main(int argc, char *argv[])
 {
@@ -190,7 +334,7 @@ int main(int argc, char *argv[])
     po::options_description options("Available Options.");
     options.add_options()("help", "Display help message")(
         "dt", po::value<double>()->default_value(0.1), "Time-step to use")(
-        "T", po::value<double>()->default_value(20.0), "Total integration time")(
+        "T", po::value<double>()->default_value(80.0), "Total integration time")(
         "Nx", po::value<int>()->default_value(100), "Number of grid points in x")(
         "Ny", po::value<int>()->default_value(100), "Number of grid points in y")(
         "ic", po::value<int>()->default_value(1),
@@ -218,29 +362,31 @@ int main(int argc, char *argv[])
     const double dx = 1.0;
     const double dy = 1.0;
 
+    // Allocate solution memories
+    double *u = new double[Nx * Ny];
+    double *v = new double[Nx * Ny];
+    double *h = new double[Nx * Ny];
+    double *h0 = new double[Nx * Ny];
+
     // MPI =====================================================
-    MPI_Init(&argc, &argv);
-    const int root = 0; // root rank
+    // MPI_Init(&argc, &argv);
+    // const int root = 0; // root rank
 
-    // Get size and rank
-    int world_rank, world_size, retval_rank, retval_size;
-    retval_rank = MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    retval_size = MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    // // Get size and rank
+    // int world_rank, world_size, retval_rank, retval_size;
+    // retval_rank = MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    // retval_size = MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    if (retval_rank == MPI_ERR_COMM || retval_size == MPI_ERR_COMM)
-    {
-        std::cout << "Invalid communicator" << std::endl;
-        return 1;
-    }
+    // if (retval_rank == MPI_ERR_COMM || retval_size == MPI_ERR_COMM)
+    // {
+    //     std::cout << "Invalid communicator" << std::endl;
+    //     return 1;
+    // }
 
     std::cout << "Goodbye World" << std::endl;
 
-
-
     // Subdomain ===============================================
-    int Nx_loc = Nx / world_size; // 3 ghost cells each side
-
-
+    // int Nx_loc = Nx / world_size; // 3 ghost cells each side
 
     // std::cout << "Nx_loc" << Nx_loc << std::endl;
     // std::cout << "Ny_loc" << Ny_loc << std::endl;
@@ -255,53 +401,49 @@ int main(int argc, char *argv[])
     // double *fv_loc = new double[Nx_loc * Ny_loc];
     // double *fh_loc = new double[Nx_loc * Ny_loc];
 
-    // // solutions
-    // double *u = nullptr;
-    // double *v = nullptr;
-    // double *h = nullptr;
+    // ======================================================
+    // test for SetInitialConditions
+    SetInitialConditions(u, v, h, h0, Nx, Ny, ic, dx, dy);
 
-    // // =========================================================
-    // // test for SetInitialConditions
-    // SetInitialConditions(u_loc, v_loc, h_loc, h0_loc, Nx_loc, Ny_loc,
-    //                      ic, dx, dy, &local_mpi_info);
+    // // debug output
+    // cout << "====== h ======" << endl;
+    // printMatrix(Nx,Ny,h);
 
-    // // ======================================================
-    // // 4th order RK Time Integrations
+    // ======================================================
+    // test for evaluating f
+    double *fu = new double[Nx * Ny];
+    double *fv = new double[Nx * Ny];
+    double *fh = new double[Nx * Ny];
 
-    // // Time advancement
-    // double time = 0.0; // start time
-    // while (time <= T)
-    // {
-    //     TimeIntegration(u_loc, v_loc, h_loc, Nx_loc, Ny_loc,
-    //                     dx, dy, dt, fu_loc, fv_loc, fh_loc, &local_mpi_info);
-    //     time += dt;
-    // }
+    // ======================================================
+    // 4th order RK Time Integrations
 
-    // GatherSolutions(u_loc, v_loc, h_loc, Nx_loc, Ny_loc, Nx, Ny, u, v, h, &local_mpi_info);
+    // Time advancement
+    double time = 0.0; // start time
+    while (time <= T)
+    {
+        TimeIntegration(u, v, h, Nx, Ny, dx, dy, dt, fu, fv, fh);
+        time += dt;
+    }
 
-    // if (local_mpi_info.world_rank == root)
-    // {
+    ofstream vOut("output.txt", ios::out | ios ::trunc);
+    vOut.precision(5);
+    for (int j = 0; j < Nx; ++j)
+    {
+        for (int i = 0; i < Ny; ++i)
+        {
+            vOut << setw(15) << i * dx << setw(15) << j * dy << setw(15) << u[i * Nx + j] << setw(15) << v[i * Nx + j] << setw(15) << h[i * Nx + j] << endl;
+        }
+    }
 
-    //     // write to file
-    //     ofstream vOut("output.txt", ios::out | ios ::trunc);
-    //     if (vOut.is_open())
-    //     {
-    //         vOut.precision(5);
-    //         for (int i = 0; i < Nx; ++i)
-    //         {
-    //             for (int j = 0; j < Ny; ++j)
-    //             {
-    //                 vOut << setw(15) << i * dx << setw(15) << j * dy << setw(15) << u[i * Ny + j] << setw(15) << v[i * Ny + j] << setw(15) << h[i * Ny + j]
-    //                      << endl;
-    //             }
-    //         }
-    //         vOut.close();
-    //     }
-    //     else
-    //     {
-    //         cout << "Unable to write to file" << endl;
-    //     }
-    // }
+    // deallocations
+    delete[] u;
+    delete[] v;
+    delete[] h;
+    delete[] h0;
+    delete[] fu;
+    delete[] fv;
+    delete[] fh;
 
     // delete[] u;
     // delete[] v;
@@ -350,6 +492,5 @@ int main(int argc, char *argv[])
     // delete[] fv;
     // delete[] fh;
 
-    MPI_Finalize();
     return 0;
 }
